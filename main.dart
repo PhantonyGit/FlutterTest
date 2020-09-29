c44ad1a8123048df1f551b2a4f63fe49cc4e27b4
phantony <32271574+PhantonyGit@users.noreply.github.com>
2020-09-29 22:16:52 +0700

    Add files via upload
    Side menu and Bottom navigation

--- main.dart
+++ main.dart
@@ -1,68 +1,88 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'nav-drawer.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tutorial Flutter',
+      title: 'Phantony.Flutter',
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+        primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: Home(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  int CurrentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //space
       appBar: AppBar(
-        title: Text('HelloWorld'),
+        title: Text("Project X"),
       ),
-      body: TestInputWidget(),
+      drawer: new Drawer(
+        child: ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text('Phantony'),
+              accountEmail: new Text("Phantony@gmail.com"),
+              currentAccountPicture: new CircleAvatar(
+                backgroundColor: Colors.blue,
+              ),
+            ),
+            new ListTile(
+              title: new Text('About Me'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (BuildContext context) => new AboutMe()));
+              },
+            )
+          ],
+        ),
+      ),
+      //space
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: CurrentIndex,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+                backgroundColor: Colors.pink),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                title: Text('Search'),
+                backgroundColor: Colors.blue),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add),
+                title: Text('Add'),
+                backgroundColor: Colors.deepPurpleAccent),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.attachment),
+                title: Text('Attach'),
+                backgroundColor: Colors.green),
+          ],
+          onTap: (index) {
+            setState(() {
+              CurrentIndex = index;
+            });
+          }),
     );
-  }
-}
-
-class TestInputWidget extends StatefulWidget {
-  @override
-  _TestInputWidgetState createState() => _TestInputWidgetState();
-}
-
-class _TestInputWidgetState extends State<TestInputWidget> {
-  final controller = TextEditingController();
-  String text = "";
-
-  @override
-  void dispose() {
-    super.dispose();
-    controller.dispose();
-  }
-
-  Void ChangeText(text) {
-    setState(() {
-      this.text = text;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      TextField(
-          controller: this.controller,
-          decoration: InputDecoration(
-              prefixIcon: Icon(Icons.message), labelText: "Type"),
-          onChanged: (text) => this.ChangeText(text)),
-      Text(this.text)
-    ]);
   }
 }
